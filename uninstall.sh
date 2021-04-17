24402851217d0be640ae464bd2e6c99f0e1ed263
Conor Shore <48555690+ConorShore@users.noreply.github.com>
2021-04-17 16:45:13 +0100

    Changed to /opt/ install location

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd ..
 sudo rm -r ~/.surfshark/
-sudo rm -r ~/SurfingPi/
+sudo rm -r /opt/SurfingPi/
 sudo rm /usr/bin/surfstart
 ls /etc/openvpn/ | grep surfshark | xargs -Ihere sudo rm /etc/openvpn/here
 echo "All removed"
