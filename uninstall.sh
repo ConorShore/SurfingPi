918872e1336a699dd18ab293888e7c49d1d650a5
Conor Shore <48555690+ConorShore@users.noreply.github.com>
2021-04-17 16:07:36 +0100

    Fixed removing the SurfingPi folder

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ..
 sudo rm -r ~/.surfshark/
-sudo rm -r SurfingPi
+sudo rm -r ~/SurfingPi/
 sudo rm /usr/bin/surfstart
 ls /etc/openvpn/ | grep surfshark | xargs -Ihere sudo rm /etc/openvpn/here
 echo "All removed"
