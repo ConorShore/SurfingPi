f487b16afd3ac813e7c6e751d07cc848ec82eee4
Conor Shore <conorshore@gmail.com>
2021-04-17 16:17:44 +0100

    Added install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+#Get all the required packages
+PREREQ_PACKAGES="curl unzip openvpn git"
+PREREQ_PACKAGES_TO_INSTALL=
+for package in $PREREQ_PACKAGES; do
+    if ! dpkg-query --show --showformat='${db:Status-Status}\n' "$package" 2> /dev/null | grep -q '^installed$'; then
+        PREREQ_PACKAGES_TO_INSTALL="$package $PREREQ_PACKAGES_TO_INSTALL"
+    fi
+done
+
+if [ "$PREREQ_PACKAGES_TO_INSTALL" ]; then
+    sudo apt update
+    sudo apt -y install $PREREQ_PACKAGES_TO_INSTALL
+fi
+
+cd /opt/
+git clone git@github.com:ConorShore/SurfingPi.git
+cd SurfingPi
+./configure
+
+
