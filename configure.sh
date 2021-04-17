e162bb41bebc0e7682433e4b4c8c723704ca4113
Conor Shore <conorshore@gmail.com>
2021-04-17 15:00:11 +0100

    intermediate update, working on interface

--- configure.sh
+++ configure.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo "SurfingPi Configure"
 echo
-echo "SurfShark Credentials"
+read -n1 -p "Do you want to update surfshark configs first? [y,n]  " doit
+case $doit in  
+  y|Y) echo "Updating configs"  
+  	./update_surf_conf.sh ;;
+  n|N) echo "Skipping update" ;; 
+  *) echo "Please answer y or n" ;; 
+esac
+echo
+echo "Surfshark Credentials"
 echo "These should be the long random looking username and password from the surfshark manual setup section"
 echo "go to https://my.surfshark.com/vpn/manual-setup/router to get these"
 echo
@@ -10,3 +18,34 @@ read -p "password:" pass_var
 
 echo $user_var > /home/$USER/surf
 echo $pass_var >> /home/$USER/surf
+echo 
+echo "Select which protocol to use"
+select proto in TCP UDP
+do
+echo "$proto selected"
+
+case $proto in
+	TCP)
+tcpendpoints=$(ls /etc/openvpn/ | grep tcp)
+select tcpendpoint in $tcpendpoints
+do
+echo "$tcpendpoint endpoint selected"
+break
+done
+
+break
+;;
+UDP)
+udpendpoints=$(ls /etc/openvpn/ | grep udp)
+select udpendpoint in $udpendpoints
+do
+echo "$udpendpoint endpoint selected"
+break
+done
+
+break
+;;
+esac
+break
+done
+
--- update_surf_conf.sh
+++ update_surf_conf.sh
@@ -3,5 +3,7 @@ sudo rm *
 sudo wget https://my.surfshark.com/vpn/api/v1/server/configurations
 sudo unzip configurations
 sudo rm configurations
+echo "Modifiying configs to use userpass file"
 sudo grep -rl "auth-user-pass" /etc/openvpn/ | sudo xargs sed -i "s>auth-user-pass>auth-user-pass /home/$USER/surf>g"
-
+echo "done"
+echo
