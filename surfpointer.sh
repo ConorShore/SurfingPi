ca2b8461187f2564c2d0ef3f8ff64a0c72eda975
ConorShore <conorshore@gmail.com>
2021-05-26 18:48:14 +0100

    again

--- surfpointer.sh
+++ surfpointer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo "SurfingPi, a Surfshark VPN Raspberry Pi help by Conor Shore 2021"
+echo "SurfingPi, a Surfshark VPN Raspberry Pi helper by Conor Shore 2021"
 
-if ["$#" -gt 0] then
+if [ "$#" -gt 0 ] then
 
     key="$1"
 
