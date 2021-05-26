4fd49638a3a2bd4214069ea61a3003a7bc795b84
ConorShore <conorshore@gmail.com>
2021-05-26 14:25:04 +0100

    removed auth store creation during vpn update

--- update_surf_conf.sh
+++ update_surf_conf.sh
@@ -4,8 +4,7 @@ sudo wget https://my.surfshark.com/vpn/api/v1/server/configurations
 sudo unzip configurations
 sudo rm configurations
 echo "Modifiying configs to use userpass file"
-mkdir /home/$USER/.surfshark
-sudo chmod -R 600 /home/$USER/.surfshark
+
 sudo grep -rl "auth-user-pass" /etc/openvpn/ | sudo xargs sed -i "s>auth-user-pass>auth-user-pass /home/$USER/.surfshark/surf>g"
 echo "done"
 echo
