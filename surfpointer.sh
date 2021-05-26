1448812e1bed6c5591bccda49c4754b943f01092
ConorShore <conorshore@gmail.com>
2021-05-26 18:39:05 +0100

    Further developing the pointer

--- surfpointer.sh
+++ surfpointer.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
+echo "SurfingPi, Surfshark Raspberry Pi help by Conor Shore 2021"
+
+if [["$#" -gt 0]]; then
+
+    key="$1"
+
+    case $key in
+    -u | --uninstall)
+        /opt/SurfingPi/uninstall.sh
+        exit
+        ;;
+    -c | --configure)
+        /opt/SurfingPi/configure.sh
+        exit
+        ;;
+    -l | --reload)
+        /opt/SurfingPi/update_surf_conf.sh
+        exit
+        ;;
+    *) # unknown option
+        echo "Unknown Option"
+        echo "-u | --uninstall  will remove SurfingPi"
+        echo "-c | --configure  will let you configure SurfingPi"
+        echo "-l | --reload     will redownload the vpn configs from surfsharl"
+        ;;
+    esac
+fi
 sudo /opt/SurfingPi/surfstart
